3a2a844945e34eafba2f37e65452437bb149a344
Gabriel Afonso <gabriel.afonso.contato@hotmail.com>
2023-09-08 12:10:27 +0200

    feat: add Progress bar

---
+++ src/pages/components/ProgressBar/index.tsx
@@ -0,0 +1,24 @@
+import { DetailedHTMLProps, ProgressHTMLAttributes } from "react";
+import useProgress from "./hooks/useProgress";
+
+type Props = DetailedHTMLProps<
+  ProgressHTMLAttributes<HTMLProgressElement>,
+  HTMLProgressElement
+>;
+
+export default function ProgressBar(props: Props) {
+  const { progress, pageHeight } = useProgress();
+
+  return (
+    <progress
+      {...props}
+      className="w-full [&::-webkit-progress-value]:rounded-r-lg [&::-webkit-progress-bar]:bg-transparent [&::-webkit-progress-value]:bg-violet-400 [&::-moz-progress-bar]:transparent"
+      aria-label="Page scroll progress"
+      tabIndex={0}
+      aria-valuenow={progress}
+      aria-valuemax={pageHeight}
+      value={progress}
+      max={pageHeight}
+    />
+  );
+}
